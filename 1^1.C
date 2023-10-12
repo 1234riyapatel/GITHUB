af5b1f018670c402e4750be0af9808990a12bbeb
Riya Patel <riyapatel6699@gmail.com>
2023-10-12 10:54:36 +0530

    Changes

--- 1^1.C
+++ 1^1.C
@@ -5,6 +5,7 @@ void main()
 //Name:Riya_Patel StudentID:23TCEDOC
 //Practical 1.1
 //To print Hello world
+// so as a output we will get Hello World!
 clrscr();
 printf("\n\t\t\tName:Riya_Patel          StudentID:23TCEDOC");
 printf("\nHello world!");
