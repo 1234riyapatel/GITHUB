4c3651f26b54b5a1ffe57dd54225c6f928c6cc3b
Riya Patel <riyapatel6699@gmail.com>
2023-10-12 10:16:22 +0530

    First commit

---
+++ 1^1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+//Name:Riya_Patel StudentID:23TCEDOC
+//Practical 1.1
+//To print Hello world
+clrscr();
+printf("\n\t\t\tName:Riya_Patel          StudentID:23TCEDOC");
+printf("\nHello world!");
+getch();
+}
---
+++ 1^2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+//Name:Riya_Patel StudentID:23TCEDOC
+//Practical 1.2
+//To print print the content with boder
+clrscr();
+printf("\t\tName:Riya_Patel          StudentID:23TCEDOC\n");
+
+printf("\n%c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c  \n",1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1);
+printf("%cIf you are resisting something,you are feeling it\t\t\t%c \n",1,1);
+printf("%c\t Any energy you fight, you are feeling it.\t\t\t%c\n",1,1);
+printf("%c\t\t if you are pushing something away,\t\t\t%c\n",1,1);
+printf("%c\t\t\t You are inviting it to stay. By Michael Singer.%c\n",1,1);
+printf("%c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c %c %c   %c   %c %c \n",1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1,1,1,1,3,1);
+getch();
+
+
+
+
+}
